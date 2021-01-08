27949f38807de448377bb114fdd2ca8a811eaa4c
Lavashick <lavashick@icloud.com>
2021-01-08 20:51:32 +0300

    Сделаны .ready(), .addClass(), .click(), .find(),

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-$("#btn-f").hide()
+$(document).ready(function() {
 
-$("p:first").click(function() {
-    $(this).hide()
-})
 
-$(".card-image").dblclick(function() {
-    $(this).hide()
-})
 
-$("p").hover(function() {
-        $(this).css("color", "green")
-    },
-    function() {
-        $(this).css("color", "black")
-    }
-)
 
-// $("h1").on({
-//     click: function() {
-//         $(this).css("color", "blue")
-//     },
-//     dblclick: function() {
-//         $(this).css("color", "yellow")
-//     },
-//     mouseenter: function() {
-//         $(this).css("color", "grey")
-//     }
-// })
 
-$("h1").on({
-    mouseenter: function() {
-        $(this).css("background-color", "lightgray")
-    },
-    mouseleave: function() {
-        $(this).css("background-color", "white")
-    },
-    click: function() {
-        $(this).css("background-color", "yellow")
-    }
+    $("#btn-f").hide()
+
+    $("p:first").click(function() {
+        $(this).hide()
+    })
+
+    $(".card-image").dblclick(function() {
+        $(this).hide()
+    })
+
+    $("p").hover(function() {
+            $(this).css("color", "green")
+        },
+        function() {
+            $(this).css("color", "black")
+        }
+    )
+
+    $("h1").on({
+        mouseenter: function() {
+            $(this).css("background-color", "lightgray")
+        },
+        mouseleave: function() {
+            $(this).css("background-color", "white")
+        },
+        click: function() {
+            $(this).css("background-color", "yellow")
+        }
+    })
+
+    $("h2").click(function() {
+        $(this).addClass("header-card")
+        $(".header-card").hide()
+    })
+
+    $(".main-footer").find("p:first").css("color", "red")
+
+
+
+
+
+
+
+
+
+
 })
