4ef2f814f1f8aa0edeabfa47f7ea69debcd95f3d
Lavashick <lavashick@icloud.com>
2021-01-08 19:50:00 +0300

    Функция on

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ $("p:first").click(function() {
 $(".card-image").dblclick(function() {
     $(this).hide()
 })
+
+$("p").hover(function() {
+        $(this).css("color", "green")
+    },
+    function() {
+        $(this).css("color", "black")
+    }
+)
+
+// $("h1").on({
+//     click: function() {
+//         $(this).css("color", "blue")
+//     },
+//     dblclick: function() {
+//         $(this).css("color", "yellow")
+//     },
+//     mouseenter: function() {
+//         $(this).css("color", "grey")
+//     }
+// })
+
+$("h1").on({
+    mouseenter: function() {
+        $(this).css("background-color", "lightgray")
+    },
+    mouseleave: function() {
+        $(this).css("background-color", "white")
+    },
+    click: function() {
+        $(this).css("background-color", "yellow")
+    }
+})
